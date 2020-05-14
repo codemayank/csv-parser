e5e555cf5cf6af8ac57575c6769a4efe5d3b7253
Mayank Yadav <mayank.yadav@pariksha.co>
2020-05-14 11:59:58 +0530

    complete csv parser

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
-function App() {
+function FileUpload({className, handleFileUpload, label}){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={className}>
+      <label>
+        {label}
+        <input
+          type="file"
+          id="file-input"
+          onChange={(e) => handleFileUpload(e)}
+          accept=".csv"
+        />
+      </label>
+    </div>
+  );
+}
+
+function App() {
+  const limit = 5;
+  let [processedData, setProcessedData] = useState(null);
+  let [offset, setOffset] = useState(0);
+
+
+  function handleFileUpload(e){
+    let [file] = e.target.files;
+    getText(file,fileReadHandler, errorHandler);
+  }
+  
+  function getText(file, fileReadHandler, errorHandler){
+    let reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = fileReadHandler;
+    reader.onerror = errorHandler;
+  
+  }
+  
+  function fileReadHandler(event){
+    let csvData = event.target.result;
+    let processedData = processCSVData(csvData);
+    setProcessedData(processedData);
+  }
+  
+  function errorHandler(){
+    alert('Error in reading file')
+  }
+  
+  function processCSVData(data){
+    let rows = data.split("\n");
+  
+    const regex = /(?:,|\n|^)("(?:(?:"")*[^"]*)*"|[^",\n]*|(?:\n|$))/gm;
+  
+    let fields = rows[0].split(regex);
+  
+    let formattedData = rows.slice(1).reduce((finalData, row) => {
+      
+      let dataPoint = row.split(regex).reduce((dp, value, index) => {
+        let fieldValue = fields[index];
+        if(fieldValue){
+          dp[fieldValue.trim()] = value;
+        }
+        return dp;
+      }, {})
+      return [...finalData, dataPoint]
+    }, []);
+  
+    return formattedData
+  }
+
+  return (
+    <div className={!processedData ? 'wrapper' : 'table-wrapper' }>
+      {!processedData ? <FileUpload className={"file-upload1"} handleFileUpload={handleFileUpload} label={'Upload Csv File'} /> : null}
+      {processedData ? (
+        <>
+          <FileUpload className={"file-upload2"} handleFileUpload={handleFileUpload} label={'Upload Another File'} />
+          <div className="button-container">
+            <button
+              onClick={() => setOffset(offset - limit)}
+              disabled={offset === 0}
+            >
+              Prev
+            </button>
+            <button
+              onClick={() => setOffset(offset + limit)}
+              disabled={offset >= Object.keys(processedData[0]).length}
+            >
+              Next
+            </button>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                {Object.keys(processedData[0])
+                  .slice(offset, limit + offset)
+                  .map((key) => (
+                    <th>{key}</th>
+                  ))}
+              </tr>
+            </thead>
+            <tbody>
+              {processedData.map((dataPoint) => (
+                <tr>
+                  {Object.keys(dataPoint)
+                    .slice(offset, limit + offset)
+                    .map((key) => {
+                      return <td>{dataPoint[key]}</td>;
+                    })}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      ) : null}
     </div>
   );
 }
